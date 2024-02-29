abd48f5789f7de64e631606aaeedbc27c75c2e47
Cecile Nguyen <ctnguyen2015@gmail.com>
2024-02-29 13:12:40 -0800

    Delete back-end directory

--- back-end/src/db/seeds/00-reservations.js
+++
@@ -1,8 +0,0 @@
-const reservations = require("./00-reservations.json")
-
-exports.seed = function (knex) {
-  return knex.raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE")
-  .then(() => {
-    return knex("reservations").insert(reservations);
-  });
-};
--- back-end/src/errors/methodNotAllowed.js
+++
@@ -1,8 +0,0 @@
-const methodNotAllowed = (req, res, next) => {
-    next({
-      status: 405,
-      message: `${req.method} not allowed for ${req.originalUrl} ::: Method Not Allowed`,
-    });
-  };
-
-  module.exports = methodNotAllowed;
